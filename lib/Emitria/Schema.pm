664d25df30786e5f634710ab99a734ff02c25e9c
Jonathan Stowe <jns+git@gellyfish.co.uk>
2014-08-25 15:56:47 +0100

    Add pod to Emitria::Schema

--- lib/Emitria/Schema.pm
+++ lib/Emitria/Schema.pm
@@ -6,10 +6,21 @@ use Moose;
 use MooseX::MarkAsMethods autoclean => 1;
 extends 'DBIx::Class::Schema';
 
-__PACKAGE__->load_namespaces;
+=head1 NAME
+
+Emitria::Schema
+
+=head1 DESCRIPTION
+
+L<DBIx::Class::Schema> sub-class for L<Emitria> storage.
+
+=cut
+
+__PACKAGE__->load_namespaces();
 
 
 
 
-__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+__PACKAGE__->meta()->make_immutable(inline_constructor => 0);
+
 1;
